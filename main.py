3efc00b3d183bee795e247703f7209d55a4044af
kyara26 <102674993+kyara26@users.noreply.github.com>
2024-10-13 20:41:52 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,65 @@
+import time
+import cv2
 from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.lang import Builder
 from kivy.config import Config
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
+from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.uix.label import Label
+from kivy.graphics.texture import Texture
 
-Config.set('graphics', 'Resizable','0')
-Config.set('graphics', 'width', '320')
-Config.set('graphics', 'height', '650')
-
+Window.size = (310, 670)
 
 class LoadingPage(Screen):
     pass
 
 class HomeMenu(Screen):
     pass
-    
+
+class FirstAidMenu1(Screen):
+    pass
+
+class FirstAidMenu2(Screen):
+    pass
+
 class CameraMenu(Screen):
     pass
 
-class FirstAidMenu(Screen):
+class BruisePage(Screen):
     pass
 
-class Q3(Screen):
+class AbrasionPage(Screen):
     pass
 
-class FinalPage(Screen):
+class BurnPage(Screen):
+    pass
+
+class MinorWoundPage(Screen):
+    pass
+
+class BleedingPage(Screen):
+    pass
+
+class SprainPage(Screen):
+    pass
+
+class MappingMenu(Screen):
     pass
 
 class ScreenManagement(ScreenManager):
     pass
 
+class WindowManager(ScreenManager):
+    pass
+
 file=Builder.load_file('FirstAidResponder.kv')
 
 class FirstAidApp(App):
-    def build(self):
+   def build(self):
         return file
-
+    
 FirstAidApp().run()
